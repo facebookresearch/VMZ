3758eceb7a29558df6e5192ae10d84b196ec9dae
Joanna Hsu <joannahsu@devgpu202.prn2.facebook.com>
2018-05-15 08:50:35 -0700

    shuffle list when create lmdb

--- scripts/create_kinetics_lmdb.sh
+++ scripts/create_kinetics_lmdb.sh
@@ -1,8 +1,11 @@
 # create lmdb database for training and testing purpose
 python data/create_video_db.py \
 --list_file=/home/joannahsu/local/R2Plus1D/process_data/kinetics/kinetics_val_full.csv \
---output_file=/data/users/joannahsu/datasets/kinetics_val
+--output_file=/data/users/joannahsu/datasets/kinetics_val_list \
+--use_list=1
 
 python data/create_video_db.py \
---list_file=/home/joannahsu/local/R2Plus1D/process_data/kinetics/kinetics_train_full_shuffle.csv \
---output_file=/data/users/joannahsu/datasets/kinetics_train
+--list_file=/home/joannahsu/local/R2Plus1D/process_data/kinetics/kinetics_train_full.csv \
+--output_file=/data/users/joannahsu/datasets/kinetics_train_list \
+--use_list=1 \
+--num_epochs=100
